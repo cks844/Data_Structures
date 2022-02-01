a99cd2a2833afa5cce127d0a8b436b576c0c6759
CYRIL  K  SONY <84973835+cks844@users.noreply.github.com>
2022-01-31 23:45:17 -0800

    Update Circular_queue.c

--- Circular_queue.c
+++ Circular_queue.c
@@ -1,108 +1,84 @@
 #include<stdio.h>
-# define MAX 10
-int cqueue[MAX];
-int front = -1;
-int rear = -1;
-void insert(int item)//TIME COMPLEXITY=O(1)
+#define MAX 10
+int cq[MAX];
+int front=-1;
+int rear=-1;
+int item;
+void enqueue(int item)//TIME COMPLEXITY=O(1)
 {
-    if((front == 0 && rear == MAX-1) || (front == rear+1))
+    if((front==0&&rear==MAX-1)||(front==rear+1))
     {
-        printf("\nQueue is Full!!\n");
-    }
-    if(front == -1)
-    {
-        front = 0;
-        rear = 0;
-    }
-    else
-    {
-        if(rear == MAX-1)
-            rear = 0;
+    printf("QUEUE IS OVERFLOW");}
+    if(rear==-1){
+        front=0;
+        rear=0;
+    }else{
+        if(rear==MAX-1)
+        rear=0;
         else
-            rear = rear+1;
-    }
-    cqueue[rear] = item ;
-    printf("Inserted -> %d\n", item);
+        rear++;
+   }
+    cq[rear]=item;
+    printf("Inserted->%d",item);
 }
-void deletion()//TIME COMPLEXITY=O(1)
+
+void dequeue()//TIME COMPLEXITY=O(1)
 {
-    if(front == -1)
-        printf("\nQueue is Empty!!\n");
-    else
+  int a;
+  if(front==-1)
+    printf("QUEUE IS UNDERFLOW");
+  else{
+    a=cq[front];
+    if(front==rear)
     {
-        printf("Deleted -> %d\n", cqueue[front]);
-        if(front == rear)
-        {
-            front = -1;
-            rear=-1;
-        }
-        else
-        {
-            if(front == MAX-1)
-                front = 0;
-            else
-                front = front+1;
-        }
+        front=-1;
+        rear=-1;}
+    else if(front==MAX-1)
+        front=0;
+    else
+        front ++;
+        printf("Deleted->%d",a);
+    }
+}
+void display()//TIME COMPLEXITY=O(n)
+{
+    int i,j;
+    if(front==-1&& rear==-1)
+    printf("QUEUE IS UNDERFLOW");
+    if(front>rear)// if front is somewhere in the middle of queue and rear is at the back of front
+    {
+        for(i=front;i<MAX;i++)
+            printf("%d\t",cq[i]);
+        for(j=front;j<=rear;j++)
+            printf("%d\t",cq[j]);
+    }
+    else{
+        for(i=front;i<=rear;i++)
+            printf("%d\t",cq[i]);
     }
 }
 
-void display()//TIME COMPLEXITY=O(n)
-{
-    int frontPos = front,rearPos = rear;
-    if(front == -1)
-    {
-     printf("\nQueue is Empty!!\n");
-    return;
-    }
-    printf("Queue elements :\n");
-    if( frontPos <= rearPos )
-    while(frontPos <= rearPos)
-    {
-    printf("%d ",cqueue[frontPos]);
-    frontPos++;
-    }
-    else
-    {
-    while(frontPos <= MAX-1)
-    {
-    printf("%d ",cqueue[frontPos]);
-    frontPos++;
-    }
-    frontPos = 0;
-    while(frontPos <= rearPos)
-    {
-    printf("%d ",cqueue[frontPos]);
-    frontPos++;
-    }
-    }
-    printf("\n");
-}
 int main()
 {
-    int choice,item;
     printf("CIRCULAR QUEUE IMPLEMENTATION USING ARRAY\n");
-    do
+    int ch=0;
+  do
+   {
+     printf("\n1:ENQUEUE\n2:DEQUEUE\n3:DISPLAY\n4:EXIT\n");
+     printf("Enter your choice:");
+     scanf("%d",&ch);
+     switch(ch)
     {
-        printf("\n1:ENQUEUE\n2:DEQUEUE\n3:DISPLAY\n4:EXIT\n");
-        printf("Enter your choice: ");
-        scanf("%d",&choice);
-        switch(choice)
-        {
-            case 1 :
-                printf("Input the element for insertion in queue : ");
-                scanf("%d", &item);
-                insert(item);
-                break;
-            case 2 :
-                deletion();
-                break;
-            case 3:
-                display();
-                break;
-            case 4:
-                break;
-            default:
-                printf("Wrong choice\n");
-        }
-    }while(choice!=4);
+        case 1:
+               printf("Enter the element to be inserted:");
+               scanf("%d",&item);
+               enqueue(item);
+               break;
+        case 2:dequeue();
+               break;
+        case 3:display();
+               break;
+        case 4:break;
+    }
+   }while(ch!=4);
 }
